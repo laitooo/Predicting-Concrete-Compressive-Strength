915cb4ea78ede5bc7056dfb71a7dd632ecfc56c2
Laitooo <killuasan25@gmail.com>
2021-02-27 13:07:36 +0200

    added data normalization

--- data.py
+++ data.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import utils
 import math
+
+np.random.seed(3)
 
 print('loading the data ...')
 data = pd.read_excel('./data.xlsx', header=1)
@@ -22,6 +25,22 @@ print('data shuffled')
 print('dividing the data ...')
 m_train = math.ceil(m * 0.75)
 train = data[0:m_train,:]
+x_train = train[:,0:6]
+y_train = train[:,6:8]
 print('training data ready with size:', train.shape)
+print('training input size:', x_train.shape)
+print('training output size:', y_train.shape)
 test = data[m_train:,:]
+x_test = test[:,0:6]
+y_test = test[:,6:8]
 print('test data ready with size:', test.shape)
+print('test input size:', x_test.shape)
+print('test output size:', y_test.shape)
+
+#print('plot shows cement before and after normalization')
+print(x_train[:,0].shape)
+#utils.plotSingleRow(x_train[:,0], "cement o.p.c")
+print('normalizing data ...')
+x_train = ((x_train - x_train.min())/(x_train.max() - x_train.min()))
+print('data normalized ')
+#utils.plotSingleRow(x_train[:,0], "cement o.p.c")
--- utils.py
+++ utils.py
@@ -1,7 +1,14 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def checkIfThereIsNan(array):
     for i in range(array.shape[0]):
         if(np.isnan(array[i]).any(axis=0)):
             return True
     return False
+
+def plotSingleRow(array, ylabel):
+    plt.figure()
+    plt.plot(array)
+    plt.ylabel(ylabel)
+    plt.show()
