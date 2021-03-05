87fccdf48a66ac8e52e9237e37124f89e9626f7c
Laitooo <killuasan25@gmail.com>
2021-03-05 19:11:30 +0200

    updated the model

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import data
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, LayerNormalization
 
-x_train, y_train, x_test, y_test, _ = data.getTestData()
+x_train, y_train, x_test, y_test, _ = data.getData()
 
 print('preparing model ...')
 
 model = Sequential()
-model.add(Dense(20, activation='relu'))
-model.add(Dense(10, activation='sigmoid'))
-model.add(Dense(5, activation='relu'))
+model.add(LayerNormalization(input_dim=6))
+model.add(Dense(20, activation='sigmoid'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(5, activation='sigmoid'))
 model.add(Dense(2))
 
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=2000, batch_size=64)
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=5000, batch_size=32, validation_data=(x_test, y_test))
 print('starting training ...')
 
 _, accuracy_train = model.evaluate(x_train, y_train)
@@ -24,3 +25,8 @@ print('model trained')
 
 print('Accuracy on train data: %.2f' % (accuracy_train*100))
 print('Accuracy on test data: %.2f' % (accuracy_test*100))
+
+predictions = model.predict(x_train)
+
+for i in range(20):
+	print('x:', x_train[i,:], ' predicted:', predictions[i], ' y:', y_train[i] )
