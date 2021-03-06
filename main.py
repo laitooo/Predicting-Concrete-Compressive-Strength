bb15f12c870644d0c938c12d4bc5368855d153a3
Laitooo <killuasan25@gmail.com>
2021-03-06 21:38:24 +0200

    updated model and added save and load

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import data
+import utils
+import os
 import numpy as np
+import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import Dense, LayerNormalization
+from keras.layers import Dense, LayerNormalization, Dropout
+
+SAVE_GRAPH = False
+SAVE_WEIGHTS = False
+LOAD_WEIGHTS = True
+
+tf.compat.v1.disable_eager_execution()
 
 x_train, y_train, x_test, y_test, _ = data.getData()
 
@@ -11,6 +20,7 @@ model = Sequential()
 model.add(LayerNormalization(input_dim=6))
 model.add(Dense(160))
 model.add(Dense(80, activation='relu'))
+model.add(Dropout(0.2))
 model.add(Dense(40, activation='sigmoid'))
 model.add(Dense(20, activation='relu'))
 model.add(Dense(10, activation='sigmoid'))
@@ -18,9 +28,21 @@ model.add(Dense(5, activation='relu'))
 model.add(Dense(2))
 
 
-model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=2000, batch_size=32, validation_data=(x_test, y_test))
+if(LOAD_WEIGHTS):
+	print('loading model weights ...')
+	output_dir = os.path.join(os.getcwd(), "concrete")
+	model.load_weights(filepath=os.path.join(output_dir, "wights.h5"))
+	print('model weights loaded')
+
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=[tf.keras.metrics.MeanSquaredError()])
+model.fit(x_train, y_train, epochs=1000, batch_size=32, validation_data=(x_test, y_test))
 print('starting training ...')
+
+if(SAVE_WEIGHTS):
+	print('saving model weights ...')
+	output_dir = os.path.join(os.getcwd(), "concrete")
+	model.save_weights(filepath=os.path.join(output_dir, "wights.h5"))
+	print('model weights saved')
 
 _, accuracy_train = model.evaluate(x_train, y_train)
 _, accuracy_test = model.evaluate(x_test, y_test)
@@ -29,7 +51,15 @@ print('model trained')
 print('Accuracy on train data: %.2f' % (accuracy_train*100))
 print('Accuracy on test data: %.2f' % (accuracy_test*100))
 
-predictions = model.predict(x_train)
+train_predictions = np.around(model.predict(x_train), 1)
+test_predictions = np.around(model.predict(x_test), 1)
 
 for i in range(20):
-	print('predicted:', predictions[i], ' real data:', y_train[i] )
+	print('train : predicted:', train_predictions[i], ' real data:', y_train[i] , ' test: predicted:', 
+		test_predictions[i], 'real data:', y_test[i])
+
+if(SAVE_GRAPH):
+	print('converting keras model to tensorflow ...')
+	output_dir = os.path.join(os.getcwd(),"concrete")
+	utils.keras_to_tensorflow(model,output_dir=output_dir,model_name="concrete.pb",log_tensorboard=True)
+	print("MODEL SAVED")
--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from keras.models import Model
+from keras.layers import *
+import os
+import tensorflow as tf
 
 def checkIfThereIsNan(array):
     for i in range(array.shape[0]):
@@ -12,3 +16,35 @@ def plotSingleRow(array, ylabel):
     plt.plot(array)
     plt.ylabel(ylabel)
     plt.show()
+
+def keras_to_tensorflow(keras_model, output_dir, model_name,out_prefix="output_", log_tensorboard=False):
+
+    if os.path.exists(output_dir) == False:
+        os.mkdir(output_dir)
+
+    out_nodes = []
+
+    print('**********************************************************************************')
+    for i in range(len(keras_model.outputs)):
+        out_nodes.append(out_prefix + str(i + 1))
+        tf.identity(keras_model.output[i], out_prefix + str(i + 1))
+        print(out_prefix + str(i + 1))
+    print('**********************************************************************************')
+
+    sess = tf.compat.v1.keras.backend.get_session()
+
+    from tensorflow.python.framework import graph_util, graph_io
+    
+
+    init_graph = sess.graph.as_graph_def()
+    main_graph = tf.compat.v1.graph_util.convert_variables_to_constants(sess, init_graph, out_nodes)
+
+    graph_io.write_graph(main_graph, output_dir, name='saved_model.pb', as_text=False)
+
+    if log_tensorboard:
+        from tensorflow.python.tools import import_pb_to_tensorboard
+
+        import_pb_to_tensorboard.import_to_tensorboard(
+            output_dir,
+            output_dir,
+            'concrete')
