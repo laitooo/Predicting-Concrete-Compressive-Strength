c04dda406da3fa4e448de2c57f31a3a48cbb0477
Laitooo <killuasan25@gmail.com>
2021-04-17 20:52:19 +0200

    completed final data

--- missing_data/complete.py
+++ missing_data/complete.py
@@ -12,7 +12,7 @@ NUM_EPOCHS = 2000
 TITILE = 'final_data3'
 COMPLETE_USING_MEAN = False
 COMPLETE_USING_MODE = False
-COMPLETE_USING_REGRESSION = False
+COMPLETE_USING_REGRESSION = True
 
 def checkIfThereIsNan(array):
     tmp = False
@@ -32,10 +32,6 @@ x = x[:,4:21] # removed (index, date, location, type of coarse aggregate)
 x = np.delete(x, 1, 1)  #removed type of fine aggregate
 x = np.delete(x, 9, 1)  #removed additive name
 x = np.delete(x, 9, 1)  #removed additive group letter
-#x = np.delete(x, 8, 1)  #removed string : type of additive
-#x = np.delete(x, 8, 1)  #removed string : dose of additive
-#x = np.delete(x, 7, 1)  #removed string : water content
-#x = np.delete(x, 11, 1) #removed int : hardened density
 
 print('\nX[0]:', x[0,:], '\nY[0]:', y[0,:])
 
@@ -96,7 +92,7 @@ if COMPLETE_USING_MODE:
 if COMPLETE_USING_REGRESSION:
     print('\nCompleting data using linear regression model')
     print('befor: x:', x.shape)
-    tmp = pd.DataFrame(x).dropna(subset=[0,1,4,5,6,7,8,9,10])
+    tmp = pd.DataFrame(x).dropna(subset=[0,1,6,7,8,9,10,11,12,13])
     x_2 = np.array(tmp)
     x_2_full = np.empty((0,x_2.shape[1]))
     x_2_missing = np.empty((0,x_2.shape[1]))
@@ -109,24 +105,22 @@ if COMPLETE_USING_REGRESSION:
     print('after: x_2:', x_2.shape, '\nx_2_full', x_2_full.shape, '\nx_2_missing', x_2_missing.shape)
 
     x_3 = np.copy(x_2_full)
-    x_3 = np.delete(x_3, 2, 1)
-    x_3 = np.delete(x_3, 2, 1)
-    y_3 = np.copy(x_2_full[:,2:4])
+    x_3 = np.delete(x_3, [2,3,4,5], 1)
+    y_3 = np.copy(x_2_full[:,2:6])
 
     x_4 = np.copy(x_2_missing)
-    x_4 = np.delete(x_4, 2, 1)
-    x_4 = np.delete(x_4, 2, 1)
-    y_4 = np.copy(x_2_missing[:,2:4])
+    x_4 = np.delete(x_4, [2,3,4,5], 1)
+    y_4 = np.copy(x_2_missing[:,2:6])
 
     model = Sequential()
-    model.add(LayerNormalization(input_dim=9))
+    model.add(LayerNormalization(input_dim=10))
     model.add(Dense(80, activation='relu'))
     model.add(Dropout(0.2))
     model.add(Dense(40, activation='sigmoid'))
     model.add(Dense(20, activation='relu'))
     model.add(Dense(10, activation='sigmoid'))
     model.add(Dense(5, activation='relu'))
-    model.add(Dense(2))
+    model.add(Dense(4))
 
     print('\n\nstarting training ...')
     model.compile(loss='mean_squared_error', optimizer='adam', metrics=[tf.keras.metrics.MeanSquaredError()])
@@ -140,19 +134,31 @@ if COMPLETE_USING_REGRESSION:
 
     for i in range(x_4.shape[0]):
         if(pd.isna(y_4[i,0])):
-            y_5[i,0] = predictions[i,0]
+            if(predictions[i,0] >= 30):
+                y_5[i,0] = 40
+            else:
+                y_5[i,0] = 20
         else:
             y_5[i,0] = y_4[i,0]
         if(pd.isna(y_4[i,1])):
             y_5[i,1] = predictions[i,1]
         else:
             y_5[i,1] = y_4[i,1]
+        if(pd.isna(y_4[i,2])):
+            y_5[i,2] = predictions[i,2]
+        else:
+            y_5[i,2] = y_4[i,2]
+        if(pd.isna(y_4[i,3])):
+            y_5[i,3] = predictions[i,3]
+        else:
+            y_5[i,3] = y_4[i,3]
 
-    complete = np.concatenate([x_3[:,0:2], y_3, x_3[:,2:11]], axis=1)
-    missing = np.concatenate([x_4[:,0:2], y_5, x_4[:,2:11]], axis=1)
+    complete = np.concatenate([x_3[:,0:2], y_3, x_3[:,2:14]], axis=1)
+    missing = np.concatenate([x_4[:,0:2], y_5, x_4[:,2:14]], axis=1)
     full_data = np.concatenate([complete, missing], axis= 0)
+    final_data = np.concatenate([full_data, y], axis= 1)
 
-    df = pd.DataFrame(full_data)
+    df = pd.DataFrame(final_data)
     filepath = 'missing_data/completed_' + TITILE + '_regression.xlsx'
     df.to_excel(filepath, index=False)
     print('saved completed data to excel file (using linear regression method)')
