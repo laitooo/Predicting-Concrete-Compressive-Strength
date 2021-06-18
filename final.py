7f3dcdb046ac8e102586fc116be8d4e50f958722
Laitooo <killuasan25@gmail.com>
2021-06-18 19:37:20 +0200

    added the latest colab file

--- final.py
+++ final.py
@@ -3,18 +3,18 @@ import data
 from utils import utils
 import numpy as np
 import tensorflow as tf
-from keras.models import Sequential
+import printing_callback as callback
 
 tf.compat.v1.disable_eager_execution()
 
 
-NUM_EPOCHS = 1000
+NUM_EPOCHS = 10000
 SAVE_INPUT = False
-SAVE_OUTPUT = True
+SAVE_OUTPUT = False
 SAVE_WEIGHTS = False
-LOAD_WEIGHTS = True
-INPUT_TITLE = 'data_files/amj_data3.xlsx'
-OUTPUT_TITLE = 'results/amj_data1.xlsx'
+LOAD_WEIGHTS = False
+INPUT_TITLE = 'data_files/amj_data3'
+OUTPUT_TITLE = 'results/amj_data'
 TRAIN_MODEL_1 = False
 TRAIN_MODEL_2 = False
 TRAIN_MODEL_3 = True
@@ -22,13 +22,13 @@ PRINT_PRDECTIONS = True
 
 
 if (TRAIN_MODEL_1):
-	train1, test1, validation1 = data.getFinalData(INPUT_TITLE)
+	train1, test1, validation1 = data.getFinalData(INPUT_TITLE + '.xlsx')
 	train1, test1, validation1 = data.prepareMultipleData(train1, test1, validation1, [16,17])
 	print('\n train1:', train1.shape)
 	xr1 = train1[:,0:14]
 	yr1 = train1[:,14:16]
 	xt1 = test1[:,0:14]
-	yt1 = test1[:,14,16]
+	yt1 = test1[:,14:16]
 	xv1 = validation1[:,0:14]
 	yv1 = validation1[:,14:16]
 	utils.exceptionIfNan(train1)
@@ -40,7 +40,7 @@ if (TRAIN_MODEL_1):
 		data.saveData(train1, 'train1.xlsx')
 
 if (TRAIN_MODEL_2):
-	train2, test2, validation2 = data.getFinalData(INPUT_TITLE)
+	train2, test2, validation2 = data.getFinalData(INPUT_TITLE + '.xlsx')
 	train2, test2, validation2 = data.prepareMultipleData(train2, test2, validation2, [14, 15, 17])
 	xr2 = train2[:,0:14]
 	yr2 = train2[:,14:15]
@@ -57,7 +57,7 @@ if (TRAIN_MODEL_2):
 		data.saveData(train2, 'train2.xlsx')
 
 if (TRAIN_MODEL_3):
-	train3, test3, validation3 = data.getFinalData(INPUT_TITLE)
+	train3, test3, validation3 = data.getFinalData(INPUT_TITLE + '.xlsx')
 	train3, test3, validation3 = data.prepareMultipleData(train3, test3, validation3, [14, 15, 16])
 	pred3 = data.getTestData([14, 15, 16])
 	xr3 = train3[:,0:14]
@@ -96,7 +96,8 @@ if (TRAIN_MODEL_1):
 	print('\n\nTrainning model 1')
 	model1.compile(loss='mean_squared_error', optimizer='adam',
 	metrics=[tf.keras.metrics.MeanAbsolutePercentageError()])
-	model1.fit(xr1, yr1, epochs=NUM_EPOCHS, batch_size=32, validation_data=(xv1, yv1), verbose=1)
+	model1.fit(xr1, yr1, epochs=NUM_EPOCHS, batch_size=32, validation_data=(xv1, yv1),
+	verbose=0, callbacks=[callback.LossAndErrorPrintingCallback()])
 
 	_, accuracy_test_1 = model1.evaluate(xt1, yt1)
 	print('\n\nmodel 1 trained')
@@ -110,7 +111,7 @@ if (TRAIN_MODEL_1):
 
 	if (SAVE_OUTPUT):
 		result = np.concatenate([xt1, yt1, test_predictions], axis=1)
-		data.saveData(result, OUTPUT_TITLE)
+		data.saveData(result, OUTPUT_TITLE + '1.xlsx')
 
 	if(SAVE_WEIGHTS):
 		print('\nsaving model weights ...')
@@ -130,7 +131,8 @@ if (TRAIN_MODEL_2):
 	print('\n\nTrainning model 2')
 	model2.compile(loss='mean_squared_error', optimizer='adam',
 	metrics=[tf.keras.metrics.MeanAbsolutePercentageError()])
-	model2.fit(xr2, yr2, epochs=NUM_EPOCHS, batch_size=32, validation_data=(xv2, yv2), verbose=1)
+	model2.fit(xr2, yr2, epochs=NUM_EPOCHS, batch_size=32, validation_data=(xv2, yv2), 
+	verbose=0, callbacks=[callback.LossAndErrorPrintingCallback()])
 
 	_, accuracy_test_2 = model2.evaluate(xt2, yt2)
 	print('\n\nmodel 2 trained')
@@ -144,7 +146,7 @@ if (TRAIN_MODEL_2):
 
 	if (SAVE_OUTPUT):
 		result = np.concatenate([xt2, yt2, test_predictions], axis=1)
-		data.saveData(result, OUTPUT_TITLE)
+		data.saveData(result, OUTPUT_TITLE + '2.xlsx')
 
 	if(SAVE_WEIGHTS):
 		print('\nsaving model weights ...')
@@ -166,7 +168,8 @@ if (TRAIN_MODEL_3):
 	print('\n\nTrainning model 3')
 	model3.compile(loss='mean_squared_error', optimizer='adam', 
 	metrics=[tf.keras.metrics.MeanAbsolutePercentageError()])
-	model3.fit(xr3, yr3, epochs=NUM_EPOCHS, batch_size=64, validation_data=(xv3, yv3), verbose=1)
+	model3.fit(xr3, yr3, epochs=NUM_EPOCHS, batch_size=64, validation_data=(xv3, yv3), 
+	verbose=0, callbacks=[callback.LossAndErrorPrintingCallback()])
 
 	_, accuracy_test_3 = model3.evaluate(xt3, yt3)
 	print('\n\nmodel 3 trained')
@@ -180,10 +183,22 @@ if (TRAIN_MODEL_3):
 
 	if (SAVE_OUTPUT):
 		result = np.concatenate([xt3, yt3, test_predictions], axis=1)
-		data.saveData(result, OUTPUT_TITLE)
+		data.saveData(result, OUTPUT_TITLE + '3.xlsx')
 
 	if(SAVE_WEIGHTS):
 		print('\nsaving model weights ...')
 		output_dir = os.path.join(os.getcwd(), "saved_wights")
 		model3.save_weights(filepath=os.path.join(output_dir, "all_data1_3.h5"))
 		print('model weights saved')
+
+
+counter = 0
+for layer in model3.layers:
+        layerWeights = []
+        for weight in layer.get_weights():
+            layerWeights.append(weight)
+            output_dir = os.path.join(os.getcwd(), "www")
+            tmp = np.array(weight)
+            data.saveData(tmp, 'omer_weights' + str(counter) + '.xlsx')
+            #print('shape:weight:', tmp.shape)
+            counter = counter + 1
